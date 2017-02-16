a57e90e06a3ed206f35739e639183d27fe1bb2b4
Nick Bartlett <nbartlett7@gmail.com>
2017-02-16 00:25:25 -0500

    Update 2017-02-16T05:25:24.076Z

--- Gulpfile.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-//////////////////////////////
-// Require Gulp and grab Armadillo
-//////////////////////////////
-const gulp = require('gulp');
-
-require('gulp-armadillo')(gulp);
