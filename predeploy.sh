a3175ff65008c65c78c5f091069881ff13c3af3b
Nick Bartlett <nbartlett7@gmail.com>
2017-02-16 03:08:14 -0500

    :bug: add missing script

---
+++ predeploy.sh
@@ -0,0 +1,1 @@
+git config --global user.email publish@travis.github && git config --global user.name "Travis" && git remote set-url origin https://${GH_TOKEN}@github.com/tteltrab/nickandmandy.wedding.git
